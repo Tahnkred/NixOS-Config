8e76edf8250b7c46c51358f3727d30a4cfa3740e
Jeremy Moreau <jeremoreau@icloud.com>
2026-08-03 19:17:46 +0200

    mh

--- Machines/Communs/Optionnels/Compositeurs/Niri.nix
+++ Machines/Communs/Optionnels/Compositeurs/Niri.nix
@@ -1,39 +1,39 @@
 { pkgs, ... }:
 
 {
-  programs.niri.enable = true;
+#   programs.niri.enable = true;
 
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.regreet}/bin/regreet";
-        user = "greeter";
-      };
-    };
-  };
+#   services.greetd = {
+#     enable = true;
+#     settings = {
+#       default_session = {
+#         command = "${pkgs.greetd.regreet}/bin/regreet";
+#         user = "greeter";
+#       };
+#     };
+#   };
 
-  # Toujours nécessaire : NixOS injecte un PATH restreint sur l'unit
-  # niri.service qui écrase le PATH complet préparé par niri-session.
-  systemd.user.services.niri.enableDefaultPath = false;
+#   # Toujours nécessaire : NixOS injecte un PATH restreint sur l'unit
+#   # niri.service qui écrase le PATH complet préparé par niri-session.
+#   systemd.user.services.niri.enableDefaultPath = false;
 
 ####################################################################
 
-#programs.niri.enable = true;
+programs.niri.enable = true;
 
-#services.greetd = {
-#  enable = true;
-#  settings = {
-#    default_session = {
-#      command = "${config.programs.niri.package}/bin/niri-session";
-#      user = "myuser";
-#    };
-#  };
-#};
+services.greetd = {
+ enable = true;
+ settings = {
+   default_session = {
+     command = "${config.programs.niri.package}/bin/niri-session";
+     user = "tahnkred";
+   };
+ };
+};
 
-# NixOS otherwise injects a stripped PATH via Environment= on the niri.service
-# unit which shadows the imported user-manager PATH. Disabling the default
-# lets niri inherit the full PATH set up by niri-session.
-#systemd.user.services.niri.enableDefaultPath = false;
+NixOS otherwise injects a stripped PATH via Environment= on the niri.service
+unit which shadows the imported user-manager PATH. Disabling the default
+lets niri inherit the full PATH set up by niri-session.
+systemd.user.services.niri.enableDefaultPath = false;
 
 }
