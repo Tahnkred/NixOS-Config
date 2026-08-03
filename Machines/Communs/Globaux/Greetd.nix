3a5e545e41a4cee20f8476e644c28e48cb4a9047
Jeremy Moreau <jeremoreau@icloud.com>
2026-08-03 16:00:26 +0200

    mise à jour du greeter pour que ça se lance...

--- Machines/Communs/Globaux/Greetd.nix
+++ Machines/Communs/Globaux/Greetd.nix
@@ -3,7 +3,7 @@
   services.greetd = {
     enable = true;
     settings = {
-      default_session.command = "${pkgs.regreet}/bin/regreet";
+      greeter = "${pkgs.regreet}/bin/regreet";
       initial_session = {
         user = "tahnkred";
         command = "${pkgs.dbus}/bin/dbus-run-session /run/current-system/sw/bin/niri";
