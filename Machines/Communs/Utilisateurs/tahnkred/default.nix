0c10ca4e7effc0cbc1a8ac1a6e37e8ac90047e4f
Jeremy Moreau <jeremoreau@icloud.com>
2026-08-05 12:28:01 +0200

    quelques modifications apportées à mon utilisateur

--- Machines/Communs/Utilisateurs/tahnkred/default.nix
+++ Machines/Communs/Utilisateurs/tahnkred/default.nix
@@ -3,7 +3,9 @@
   users.users."tahnkred" = {
     isNormalUser = true;
     description = "Tancrède";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = ifTheyExist [ "networkmanager" "wheel" ];
+
+    packages = [pkgs.home-manager];
   };
 home-manager.users.tahnkred = import ../../../../Home/Tahnkred/Home-Machines/${config.networking.hostName}.nix;
 }
