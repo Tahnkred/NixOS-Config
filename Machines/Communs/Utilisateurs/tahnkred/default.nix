a546fc9ddb56ad7b1e73f3b40bface3af81b9bbd
Jeremy Moreau <jeremoreau@icloud.com>
2026-08-05 13:08:32 +0200

    ajout d'une commande valeur manquée è-è

--- Machines/Communs/Utilisateurs/tahnkred/default.nix
+++ Machines/Communs/Utilisateurs/tahnkred/default.nix
@@ -1,4 +1,8 @@
 { pkgs, config, lib,... }:
+let
+  ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+
+in
 {
   users.users."tahnkred" = {
     isNormalUser = true;
