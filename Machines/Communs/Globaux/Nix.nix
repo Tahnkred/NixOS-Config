527974c697a006d7672911ac88436eb25e9671fa
Jeremy Moreau <jeremoreau@icloud.com>
2026-08-06 15:13:06 +0200

    ajout de clefs dans /Machines/Communs/Globaux/Nix.nix

--- Machines/Communs/Globaux/Nix.nix
+++ Machines/Communs/Globaux/Nix.nix
@@ -6,10 +6,20 @@ let
 in {
     nix = {
         settings = {
+            extra-substituters = lib.mkAfter [
+                "https://noctalia.cachix.org"
+                "https://niri.cachix.org"
+                ];
+            extra-trusted-public-keys = [
+                "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+                "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+                ];
+
             trusted-users = [
                 "root"
                 "@wheel"
             ];
+
         auto-optimise-store = lib.mkDefault true;
         experimental-features = [
             "nix-command"
@@ -17,13 +27,14 @@ in {
             "ca-derivations"
         ];
 
-    #  warn-dirty = false;
-    #  system-features = [
-    #    "kvm"
-    #    "big-parallel"
-    #    "nixos-test"
-    #  ];
-    #  flake-registry = ""; # Disable global flake registry
+     warn-dirty = false;
+
+     system-features = [
+       "kvm"
+       "big-parallel"
+       "nixos-test"
+     ];
+     flake-registry = ""; # Disable global flake registry
     };
 
     gc = {
@@ -34,7 +45,7 @@ in {
     };
 
     # Add each flake input as a registry and nix_path
-    #registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    #nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
     };
 }
