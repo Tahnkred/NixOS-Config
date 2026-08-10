ccadcc51d448e8166f0ccb4c58b60ae0115ee216
Jeremy Moreau <jeremoreau@icloud.com>
2026-08-10 10:07:34 +0200

    modifications pour Github (sinon ça bug)

--- Home/Tahnkred/Fonctionnels/Applications/Developpement/vscode.nix
+++ Home/Tahnkred/Fonctionnels/Applications/Developpement/vscode.nix
@@ -2,11 +2,11 @@
 {
   programs.vscode = {
     enable = true;
-    argvSettings = {
-      "password-store" = "gnome-libsecret";
-    };
+    # argvSettings = {
+    #   "password-store" = "gnome-libsecret";
+    # };
   };
-  # xdg.configFile."Code/User/argv.json".text = builtins.toJSON {
-  # password-store = "gnome-libsecret";
-  # };
+  xdg.configFile."Code/User/argv.json".text = builtins.toJSON {
+  password-store = "gnome-libsecret";
+  };
 }
