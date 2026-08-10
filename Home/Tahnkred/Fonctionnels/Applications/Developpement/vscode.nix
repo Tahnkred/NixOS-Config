0fdc0f3e4b2d95cd7d4ab17d7c4074f7006f155f
Jeremy Moreau <jeremoreau@icloud.com>
2026-08-10 12:27:41 +0200

    ajout commentaire

--- Home/Tahnkred/Fonctionnels/Applications/Developpement/vscode.nix
+++ Home/Tahnkred/Fonctionnels/Applications/Developpement/vscode.nix
@@ -6,6 +6,8 @@
     #   "password-store" = "gnome-libsecret";
     # };
   };
+
+  #Il faut lancer une première fois via le terminal avec la commande 'code --password-store="gnome-libsecret"'.
   xdg.configFile."Code/User/argv.json".text = builtins.toJSON {
   password-store = "gnome-libsecret";
   };
